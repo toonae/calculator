e92fe5847ab396ce6b1fa2b82740b951a99e2aa4
supanida <sup.ta1909@gmail.com>
2018-02-21 11:43:58 +0700

    ADD Complete

--- app/src/main/java/sdu/cs58/supanida/calculator/MainActivity.java
+++ app/src/main/java/sdu/cs58/supanida/calculator/MainActivity.java
@@ -2,12 +2,45 @@ package sdu.cs58.supanida.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //1.Explicit ประกาศตัวแปรบน java
+    EditText number1EditText, number2EditText;
+    Button addButton;
+    TextView resultTextView;
+
+    int num1,num2,result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    //2.Initial view ผูกตัวแปรบน java กับอิลิเม้นท์บน XML
+    number1EditText = findViewById(R.id.edtNumber1);
+    number2EditText = findViewById(R.id.edtNumber2);
+    addButton = findViewById(R.id.btnAdd);
+    resultTextView = findViewById(R.id.txvResult);
+    //3.สั่งให้ปุ่มดักฟังว่าเมื่อ user คลิกที่ปุ่ม Login
+    addButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //แปลงค่าที่รับจาก EditText เป็นตัวเลข แล้วเก็บลงในตัวแปรชนิด Int
+        num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+        num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+        result = num1+num2;
+
+        //4.แสดงผลลัพธ์การคำนวณ
+        resultTextView.setText(result+"");
+
+        }
+    }); //end setOnClickListener
+
+    } //end Method onCreate
+
+}//end Class
+
